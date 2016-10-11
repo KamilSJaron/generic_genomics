cb09d32f885749c220003f11c4ac00491f904ed1
KamilSJaron <kamil.jaron@unil.ch>
2016-10-11 10:10:27 +0000

    fasta2genomic_stats for computation of basic stats

---
+++ fasta2genomic_stats.py
@@ -0,0 +1,27 @@
+import sys
+from Bio import SeqIO 
+
+lengths = []
+
+for seq_record in SeqIO.parse(sys.argv[1], "fasta"):
+    lengths.append(len(seq_record))
+
+lengths.sort(reverse=True)
+
+if len(sys.argv) == 3:
+    half_of_genome = int(sys.argv[2]) / 2
+else:
+    half_of_genome = sum(lengths) / 2
+
+cumsum = 0
+L50 = 0
+
+for seq_len in lengths:
+    cumsum += seq_len
+    L50 += 1
+    if cumsum > half_of_genome:
+        print("Total sum of lengths: ", sum(lengths))
+        print("Number of records: ", len(lengths))
+        print("N50 is: ", seq_len)
+        print("L50 is: ", L50)
+        break;
