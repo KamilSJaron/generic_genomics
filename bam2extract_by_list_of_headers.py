d3c1f8fb12985e183100cab974b150b04aab5906
KamilSJaron <kamil.jaron@unil.ch>
2016-12-12 22:43:02 +0000

    headers are not filtered, but it at least it works

--- bam2extract_by_list_of_headers.py
+++ bam2extract_by_list_of_headers.py
@@ -6,30 +6,31 @@ import pysam
 # funky_scafolds.list
 # ../contig_filtering/Tge_pe_700.bam
 samfile = pysam.AlignmentFile(sys.argv[1], "rb")
+filtered = pysam.AlignmentFile(sys.argv[3], "wb", template=samfile)
 # header set
 header_set = set(line.strip() for line in open(sys.argv[2]))
 original_headers = samfile.header
+#new_headers = dict()
 
 # headers
 # @HD line
-head = original_headers['HD']
-print('@HD\tVN:{:s}\tSO:{:s}'.format(head['VN'], head['SO']))
-
+#new_headers['HD'] = original_headers['HD']
 # @SQ lines
-for reference in original_headers['SQ']:
-	if reference['SN'] in header_set:
-		print('@SQ\tSN:{:s}\tLN:{:s}'.format(reference['SN'], str(reference['LN'])))
-
+#new_headers['SQ'] = list()
+#for reference in original_headers['SQ']:
+#	if reference['SN'] in header_set:
+#		new_headers['SQ'].append(reference)
 # @PG line
-program = original_headers['PG'][0]
-print('@PG\tVN:{:s}\tID:{:s}\tCL:{:s}'.format(program['VN'], program['ID'], program['CL']))
+#new_headers['PG'] = original_headers['PG']
+
+# filtered = pysam.AlignmentFile(sys.argv[3], "wb", header=new_headers)
 
 # print entries in list
 for entry in samfile:
 	try:
 		if entry.reference_name in header_set:
-			print( str(entry))
-	except (ValueError, TypeError) as e:
+			filtered.write(entry)
+	except (ValueError, TypeError):
 		continue;
 
 samfile.close()
