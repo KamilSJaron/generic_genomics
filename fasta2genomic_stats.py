7cb9ccd9cd0e7db3fef5a5c6a31cb01473fe8a71
Kamil S Jaroň <376090@mail.muni.cz>
2017-02-14 18:36:48 +0100

    genomic stats updated for N10 - 80

--- fasta2genomic_stats.py
+++ fasta2genomic_stats.py
@@ -1,5 +1,5 @@
 import sys
-from Bio import SeqIO 
+from Bio import SeqIO
 
 lengths = []
 
@@ -9,24 +9,29 @@ for seq_record in SeqIO.parse(sys.argv[1], "fasta"):
 lengths.sort(reverse=True)
 
 if len(sys.argv) == 3:
-    half_of_genome = int(sys.argv[2]) / 2
+    tenth_of_genome = int(sys.argv[2]) / 10
 else:
-    half_of_genome = sum(lengths) / 2
+    tenth_of_genome = sum(lengths) / 10
 
-if(half_of_genome > sum(lengths)):
-    print("Half of the specified genome length:", half_of_genome)
+if(tenth_of_genome * 8 > sum(lengths)):
+    print("80\% of the specified genome length:", tenth_of_genome * 8)
     print("is greater than total sum of sequences in fasta: ", sum(lengths))
     sys.exit()
 
 cumsum = 0
-L50 = 0
+LX = 0
+
+print("Total sum of lengths:\t" + str(sum(lengths)))
+print("Number of records:\t" + str(len(lengths)))
+
+# X is just an X / 10 from NX. for X = 1; it computes N10, for X = 2 it computes N20 etc.
+X = 1
 
 for seq_len in lengths:
     cumsum += seq_len
-    L50 += 1
-    if cumsum > half_of_genome:
-        print("Total sum of lengths: ", sum(lengths))
-        print("Number of records: ", len(lengths))
-        print("N50 is: ", seq_len)
-        print("L50 is: ", L50)
-        break;
+    LX += 1
+    if cumsum > tenth_of_genome * X:
+        print("L" + str(X) + "0 and N" + str(X) + "0 are: " + str(LX) + "\t" + str(seq_len))
+        if X == 8:
+            break;
+        X += 1;
