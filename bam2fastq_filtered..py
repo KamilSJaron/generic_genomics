87436545496d9b4950ac28ebecfc7adccceb5f4f
Kamil S Jaroň <376090@mail.muni.cz>
2017-10-27 16:00:40 +0200

    script generalized to be able to filter or keep of one of both mates are mapping

--- bam2fastq_filtered..py
+++ bam2fastq_filtered..py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # based on Devon Ryan's script https://github.com/dpryan79/Answers/tree/master/bioinfoSE_2149
+# potential speed-up would be with asking for sorted bam file on input
 
 import pysam
 import argparse
@@ -9,10 +10,17 @@ import gzip
 
 
 def parseArgs():
-    parser = argparse.ArgumentParser(description="Convert a paired-end BAM file to fastq files after filtering out pairs where both align to a list of filtered contigs.")
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description="Convert an indexed paired-end BAM file to fastq files after filtering out pairs where both align to a list of filtered contigs.")
     parser.add_argument("BAMfile", help="The BAM file to parse")
-    parser.add_argument("keepList", help="A file containing a list of contigs/scaffolds/chromosomes to keep (one per line). Pairs of reads will only be kept if both of mates align to one of these.")
+    parser.add_argument("refList", help="A file containing a list of contigs/scaffolds/chromosomes to keep/filter (one per line).")
     parser.add_argument("outputPrefix", help="The prefix for the output files. They will be names prefix_R1.fastq.gz and prefix_R2.fastq.gz")
+
+    parser.add_argument("--keep", action='store_const', const=False,
+                        help="if the List represents a set of contigs that should be kept, default is a set of contigs to be filtered.")
+    parser.add_argument("--both", action='store_const', const=False,
+                        help="keep/filter if both in a pair are mapping to contigs in the list, default is to keep/filter if one of pair maps to contigs in the list.")
+
     return parser
 
 
@@ -22,20 +30,57 @@ def loadFilterList(fname):
         s.add(line.strip())
     return s
 
+# Always exclude secondary/supplemental alignments : if b.flag & 2304
 
-def filterAlignment(b, keepList):
+def filterOne(b, refList):
     """
-    Exclude only cases where BOTH mates align to a filtered scaffold/contig
+    Exclude cases where ONE of mates align to a FILTERED scaffold/contig
+    """
+    if b.flag & 2304:
+        return True
+    if b.is_unmapped and b.mate_is_unmapped:
+        return False
+    if b.reference_name in refList or b.next_reference_name in refList:
+        return True
+    return False
+
+def filterBoth(b, refList):
+    """
+    Exclude only cases where BOTH mates align to a FILTERED scaffold/contig
+    """
+    if b.flag & 2304:
+        return True
+    if b.is_unmapped or b.mate_is_unmapped:
+        return False
+    if b.reference_name in refList and b.next_reference_name in refList:
+        return True
+    return False
+
+def keepOne(b, refList):
+    """
+    KEEP only cases where ONE of mates align to a one of listed scaffolds/contigs
     Additionally exclude secondary/supplemental alignments
     """
     if b.flag & 2304:
         return True
     if b.is_unmapped and b.mate_is_unmapped:
         return True
-    if b.reference_name in keepList or b.next_reference_name in keepList:
+    if b.reference_name in refList or b.next_reference_name in refList:
         return False
     return True
 
+def keepBoth(b, refList):
+    """
+    KEEP only cases where BOTH of mates align to any of listed scaffolds/contigs
+    Additionally exclude secondary/supplemental alignments
+    """
+    if b.flag & 2304:
+        return True
+    if b.is_unmapped or b.mate_is_unmapped:
+        return True
+    if b.reference_name in refList and b.next_reference_name in refList:
+        return False
+    return True
 
 def inBuffer(b, buf):
     if b.query_name in buf:
@@ -70,7 +115,7 @@ def dumpAlignments(b, buf, of1, of2):
     if b2[2] == 2:
         of = of2
     of.write("@{}\n{}\n+\n{}\n".format(b.query_name, b2[0], b2[1]))
-    
+
     # Write the read
     s = b.query_sequence
     q = ''.join([chr(c+33) for c in b.query_qualities])
@@ -90,13 +135,24 @@ def main(args):
     args = parseArgs().parse_args(args)
 
     bam = pysam.AlignmentFile(args.BAMfile)
-    keepList = loadFilterList(args.keepList)
+    refList = loadFilterList(args.refList)
     of1 = gzip.GzipFile("{}_R1.fastq.gz".format(args.outputPrefix), "w")
     of2 = gzip.GzipFile("{}_R2.fastq.gz".format(args.outputPrefix), "w")
 
+    if keep:
+        if both :
+            filterAlignment = keepBoth
+        else :
+            filterAlignment = keepOne
+    else :
+        if both :
+            filterAlignment = filterBoth
+        else :
+            filterAlignment = filterOne
+
     buf = dict()
     for b in bam:
-        if filterAlignment(b, keepList):
+        if filterAlignment(b, refList):
             continue
         if inBuffer(b, buf):
             dumpAlignments(b, buf, of1, of2)
