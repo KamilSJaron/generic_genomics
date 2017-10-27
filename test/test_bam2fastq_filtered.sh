b34352d49afe9a235d0587f27f9e9ca2e074eb7c
Kamil S Jaroň <376090@mail.muni.cz>
2017-10-27 19:22:47 +0200

    the test improved

--- test/test_bam2fastq_filtered.sh
+++ test/test_bam2fastq_filtered.sh
@@ -10,21 +10,27 @@ echo "1_Tdi_b3v04_scaf000005" >> list_to_test
 ../bam2fastq_filtered.py --keep ../test/data/ten_reads_map.bam list_to_test keep_one
 ../bam2fastq_filtered.py --keep --both ../test/data/ten_reads_map.bam list_to_test keep_both
 
-if [[ $(zcat filt_one_R1.fastq.gz | wc -l) -eq 24 ]]; then
-   echo "filter one works";
+if [[ $(zcat filt_one_R1.fastq.gz | wc -l) -ne 24 ]]; then
+   echo "filter one DOES NOT work";
+   exit 1
 fi
 
-if [[ $(zcat filt_both_R1.fastq.gz | wc -l) -eq 28 ]]; then
-   echo "filter one works";
+if [[ $(zcat filt_both_R1.fastq.gz | wc -l) -ne 28 ]]; then
+   echo "filter both DOES NOT work";
+   exit 1
 fi
 
-if [[ $(keep_one_R1.fastq.gz | wc -l) -eq 16 ]]; then
-   echo "filter one works";
+if [[ $(zcat keep_one_R1.fastq.gz | wc -l) -ne 16 ]]; then
+   echo "keep one DOES NOT work";
+   exit 1
 fi
 
-if [[ $(zcat keep_both_R1.fastq.gz | wc -l) -eq 12 ]]; then
-   echo "filter one works";
+if [[ $(zcat keep_both_R1.fastq.gz | wc -l) -ne 12 ]]; then
+   echo "keep both DOES NOT work";
+   exit 1
 fi
 
-# cd ..
-# rm -r playground
+echo "Everything works, cleaning"
+
+cd ..
+rm -r playground
